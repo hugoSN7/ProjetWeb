0cc45e7139cf1ed0dca1e6a1b8e962c94c03e53e
Ternardin <hugoSN7>
2022-04-09 16:57:30 +0200

    ajout des classes de la db

---
+++ MemeGenerator/src/classes/Tag.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Tag {
+    
+    private String mot;
+
+    public Tag(String mot){
+        this.mot = mot;
+    }
+
+    public String getMot() {
+        return mot;
+    }
+
+    public void setMot(String mot) {
+        this.mot = mot;
+    }
+
+}
