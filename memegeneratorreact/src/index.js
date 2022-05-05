61206b1de5ab17cc3ad22bfdaf231dacb98fd2d5
CedriCazanove <cedricazanove@hotmail.fr>
2022-05-05 22:54:00 +0200

    ajout route

--- memegeneratorreact/src/index.js
+++ memegeneratorreact/src/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './Main';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Generate from "./pages/Generate";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
-ReactDOM.render(
-    <Main />,
-  document.getElementById('root')
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Generate />} />
+          <Route path="home" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ memegeneratorreact/src/pages/Generate.js
@@ -0,0 +1,5 @@
+const Generate = () => {
+  return <h1>Generate</h1>;
+};
+
+export default Generate;
