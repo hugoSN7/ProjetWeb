1483ba7ca26c6ab81097dd5be9cf2df4bd3b6cfb
hugoSN7 <hugo.casuzanne@gmail.com>
2022-05-15 16:15:01 +0200

    les url en haut à gauche

--- memegeneratorreact/src/pages/Layout.js
+++ memegeneratorreact/src/pages/Layout.js
@@ -10,15 +10,19 @@ const Layout = () => {
         <ul>
           <li>
             <Link to="/home" class="urlHome">Home</Link>
+            <p class="trait1">|</p>
           </li>
           <li>
             <Link to="/" class="urlGenerate">Generate</Link>
+            <p class="trait2">|</p>
           </li>
           <li>
             <Link to="/login" class="urlLogin">Login</Link>
+            <p class="trait3">|</p>
           </li>
           <li>
             <Link to="/about" class="urlAbout">About</Link>
+            <p class="trait4">|</p>
           </li>
           <li>
             <Link to="/contact" class="urlContact">Contact</Link>
