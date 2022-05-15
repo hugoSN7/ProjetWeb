1ed8a2be6eab00f1a0f0f15a7d8654881318d508
CedriCazanove <cedricazanove@hotmail.fr>
2022-05-15 14:32:27 +0200

    Changement sur Generator

--- memegeneratorreact/src/pages/Generate.js
+++ memegeneratorreact/src/pages/Generate.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect, Component } from "react";
 import {Link} from "react-router-dom";
 import UploadImage from "./UploadImage";
-import '../WebContent/css/General.css';
-import '../WebContent/css/Generate.css';
 
 function Generate() {
     return (
-        <div style={{ textAlign: "center" }} class="generate">
+        <div style={{ textAlign: "center" }}>
             <UploadImage />
         </div>
     )
