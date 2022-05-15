26cd13faf85d099b3f17a56da40d6678a61ee8ae
Neav Jade <jneav@n7-lnx-mate.ad.inp-toulouse.fr>
2022-05-15 16:50:26 +0200

    modification de Home.js (début de la mosaique)

--- memegeneratorreact/src/pages/Home.js
+++ memegeneratorreact/src/pages/Home.js
@@ -1,10 +1,49 @@
-import '../WebContent/css/General.css';
-import '../WebContent/css/Home.css';
+import React, { useState, useEffect, Component } from "react";
+import {Link} from "react-router-dom";
+import Zero from "../HomeComponents/Zero";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
+class Home extends React.Component {
 
-const Home = () => {
-  return <h1 class="home">Home</h1>;
-};
+  state = {
+    items: Array.from({ length: 200 })
+  };
+
+  fetchMoreData = () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 200 }))
+      });
+    }, 1500);
+  };
+
+  render() 
+  {
+    return (
+      <div>
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {this.state.items.map((i, index) => (
+            <div key={index}>
+              div - #{index}
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+    );
+  }
+
+}
 
 export default Home;
+
+
+
+
 
