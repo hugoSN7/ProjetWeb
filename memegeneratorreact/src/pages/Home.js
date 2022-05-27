341507cbaa9bce305bcf0c5ac90b276ba73bda93
hugoSN7 <hugo.casuzanne@gmail.com>
2022-05-27 23:23:12 +0200

    le home de Jade

--- memegeneratorreact/src/pages/Home.js
+++ memegeneratorreact/src/pages/Home.js
@@ -1,10 +1,47 @@
-import React, { Component }  from 'react';
 import '../WebContent/css/General.css';
 import '../WebContent/css/Home.css';
+import React, { useState, useEffect, Component } from "react";
+import {Link} from "react-router-dom";
+import Zero from "../HomeComponents/Zero";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-const Home = () => {
-  return <h1 class="home">Home</h1>;
-};
+class Home extends React.Component {
+
+  state = {
+    items: Array.from({ length: 200 })
+  };
+
+  fetchMoreData = () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 200 }))
+      });
+    }, 1500);
+  };
+
+  render() 
+  {
+    return (
+      <div>
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {this.state.items.map((i, index) => (
+            <div key={index}>
+              div - #{index}
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+    );
+  }
+
+}
 
 export default Home;
 
