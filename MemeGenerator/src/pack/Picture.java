fd33b6ba326c89fb71c56b366262b3004aac99a1
CedriCazanove <83009910+CedriCazanove@users.noreply.github.com>
2022-05-28 15:25:13 +0200

    Add files via upload

--- MemeGenerator/src/pack/Picture.java
+++ MemeGenerator/src/pack/Picture.java
@@ -1,27 +1,31 @@
 package pack;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 @Entity
 public class Picture {
 	
 	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)  
-	int id;
 	private String namePicture;
+	
 	private String path;
+	private Boolean isMeme;
 	
-	@OneToMany(mappedBy="picture", fetch = FetchType.EAGER)
-	Collection<Tag> tags;
-	
-	
+	@ManyToMany
+	private Collection<Tag> tags = new ArrayList<Tag>();
+		
 	public String getNamePicture() {
 		return namePicture;
 	}
@@ -39,5 +43,17 @@ public class Picture {
 	}
 	public void setTags(Collection<Tag> tags) {
 		this.tags = tags;
+	}
+	public Boolean getIsMeme() {
+		return isMeme;
+	}
+	public void setIsMeme(Boolean isMeme) {
+		this.isMeme = isMeme;
+	}
+	public String toString() {
+		String str = namePicture;
+		str += "\n\t path : " + path;
+		str += "\n\t isMeme : " + isMeme;
+		return str;
 	}
 }
